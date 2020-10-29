88900de948c952ec6444ea25fc8b4186be8bf3e3
camshaft54 <cam.shaw1234@gmail.com>
2020-10-29 09:53:48 -0700

    Complete Student.java

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,26 @@
-public class Student {
-    
+public class Student extends Person {
+
+    private int classYear;
+
+    public Student(String firstName, String lastName, int year) {
+        super(firstName, lastName);
+        classYear = year;
+    }
+
+    public Student() {
+        classYear = 2020;
+    }
+
+    public void setClassYear(int year) {
+        classYear = year;
+    }
+
+    public int getClassYear() {
+        return classYear;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + classYear;
+    }
 }
--- src/main/java/StudentClient.java
+++ src/main/java/StudentClient.java
@@ -1,5 +1,10 @@
 public class StudentClient {
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+        System.out.println(s1.toString());
+        Student s2 = new Student("Cam", "Shaft", 1854);
+        System.out.println(s2.toString());
+        s2.setClassYear(1864);
+        System.out.println(s2.getClassYear());
     }
 }
